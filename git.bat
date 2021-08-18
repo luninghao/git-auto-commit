4fcd5c11bb7139d4f2cce2ed3d5a02a9e28151b8
luninghao <708886514@qq.com>
2021-08-18 18:43:27 +0800

    删除文件

--- git.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@title bat 交互执行git命令
-E:
-cd E:/git-auto-commit
-git add .
-git commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日
-git push -u origin main
--- push.sh
+++
@@ -1,3 +0,0 @@
-git pull && /root/.nvm/versions/node/v6.6.0/bin/node add.js && git commit -a -m 'git auto commit' && git push origin master
-
-
